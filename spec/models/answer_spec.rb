f9bec76bf95a19ccc1c216a098b598346009391c
rubygitflow <nrubygitflow@gmail.com>
2020-07-09 04:03:09 +0300

    answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Question, type: :model do
+RSpec.describe Answer, type: :model do
   describe 'associations' do
-    it { should have_many(:answers) }
+    it { should belong_to(:question) }
   end
 
   describe 'validations' do
-    it {should validate_presence_of :title}
     it {should validate_presence_of :body}
   end
 end
