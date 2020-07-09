a0cd557353ce29e30659e92310c4bf41c68440f5
rubygitflow <nrubygitflow@gmail.com>
2020-07-09 15:50:46 +0300

    add test.dependent

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   describe 'associations' do
     it { should have_many(:answers) }
+    # http://matchers.shoulda.io/docs/v4.3.0/Shoulda/Matchers/ActiveRecord.html#have_many-instance_method
+    it { should have_many(:answers).dependent(:destroy) }
   end
 
   describe 'validations' do
