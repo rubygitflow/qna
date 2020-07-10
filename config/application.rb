cddea6c9a64c64954147feeb08b54dbd934f4a9d
rubygitflow/qna <nrubygitflow@gmail.com>
2020-07-10 03:47:31 +0300

    add questions controller

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module Qna
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false,
-                       request_specs: false
+                       request_specs: false,
+                       controller_specs: true
     end
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -3,4 +3,10 @@ FactoryBot.define do
     title { "MyString" }
     body { "MyText" }
   end
+
+  # Fix KeyError: Trait not registered: "invalid"
+  # https://stackoverflow.com/questions/61124466/rspec-trait-not-registered-error-when-using-faker
+  trait :invalid do
+    title { nil }
+  end  
 end
