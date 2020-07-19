92532799a317b02d8f109fe295b4c1233ad87487
rubygitflow/qna <nrubygitflow@gmail.com>
2020-07-19 16:54:53 +0300

    add 8_tdd

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,7 @@ class Question < ApplicationRecord
   # https://apidock.com/rails/ActiveRecord/Associations/ClassMethods/has_many
   has_many :answers, -> { order('best DESC, created_at') }, dependent: :destroy
 
+  has_many_attached :files
+
   validates :title, :body, presence: true
 end
