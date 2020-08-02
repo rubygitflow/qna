45ec86ce88e3f95bb6bf4f0ae738e8c0fc0ce632
rubygitflow/qna <nrubygitflow@gmail.com>
2020-08-02 08:16:34 +0300

    apply the service object pattern

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,20 +12,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:github]
 
   def self.find_for_oauth(auth)
-    authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
-    return  authorization.user if authorization
-
-    email = auth.info[:email]
-    user = User.find_by(email: email)
-    if user
-      user.create_authorization(auth)
-    else
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email: email, password: password, password_confirmation: password)
-      user.create_authorization(auth)
-    end
-
-    user
+    FindForOauth.new(auth).call
   end
 
   def author?(resource)
@@ -35,7 +22,7 @@ class User < ApplicationRecord
   def voted?(resource)
     resource.votes.exists?(user_id: id)
   end
-  
+
   def create_authorization(auth)
     authorizations.create(provider: auth.provider, uid: auth.uid.to_s)
   end
